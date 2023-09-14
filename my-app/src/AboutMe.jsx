2963cd5f6d234c57965164ad0d5f6d37229f0ce0
Mugesh <mushi025@gmail.com>
2023-09-14 09:45:41 +0100

    About-Me Changes

--- my-app/src/AboutMe.jsx
+++ my-app/src/AboutMe.jsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 import MyPhoto from "./images/Mugesh.png";
+import { gsap, Power4 } from "gsap";
+import { TextPlugin } from "gsap/all";
+
+gsap.registerPlugin(TextPlugin);
 
 function AboutMe() {
+  let marker = useRef(null);
+  let texts = useRef(null);
+  const words = ["Developer", "Graphic Designer"];
+
+  useEffect(() => {
+    gsap.to(marker, {
+      opacity: 0,
+      repeat: -1,
+      yoyo: true,
+      duration: 0.5,
+      ease: Power4.easeInOut,
+    });
+
+    let tlMaster = gsap.timeline({ repeat: -1 });
+    words.forEach((word) => {
+      let tlText = gsap.timeline({ repeat: 1, yoyo: true });
+      tlText.to(texts, { duration: 1, text: word });
+      tlMaster.add(tlText);
+    });
+  }, [texts, words]);
+
   return (
     <div>
       <div className="">
         <div className="about-me w-2/3 flex">
           <img src={MyPhoto} alt="Mugesh" id="my_photo" className="w-1/3" />
           <h1>I'm a</h1>
-          <h1>Developer & Graphic Designer</h1>
+          <div
+            ref={(item) => {
+              texts = item;
+            }}
+          ></div>
+          <span
+            ref={(item) => {
+              marker = item;
+            }}
+          >
+            |
+          </span>
           <p>
             Grounded and Solution-oriented Machine Learning Enthusiast with a
             strong determination to help others through modern technologies.
